5a960c3551469b74b3540fa53c89460398a0eb98
bjh <18566300@qq.com>
2020-01-22 00:00:52 +0800

    偏导数

--- latex_graphics.py
+++ latex_graphics.py
@@ -1,6 +1,6 @@
 from sympy import *
 import matplotlib.pyplot as plt
-from matplotlib import rc, rcParams
+from matplotlib import rc
 
 a, b, c, d = symbols('a b c d')
 M = Matrix([[a, b],
@@ -8,14 +8,13 @@ M = Matrix([[a, b],
 
 print(M)
 print(M.T)
-lat = latex(M.inv())
+lat = latex(M.T @ M)
 print(lat)
 
-# tex = r"$\sum_{n=1}^\infty\frac{-e^{i\pi}}{2^n}!$"
 tex = "$"+lat+"$"
 rc('font', **{'family': 'sans-serif', 'sans-serif': ['Helvetica']})
 rc('text', usetex=True)
 rc('text.latex', preamble=r'\usepackage{amsmath}')
-plt.text(0.5, 0.5, tex, size=40)
+plt.text(0.05, 0.5, tex, size=40)
 plt.axis('off')
 plt.show()
