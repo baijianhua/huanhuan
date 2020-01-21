1f87d99b93294472c74391b93350bf9698a494e1
bjh <8566300@qq.com>
2020-01-21 13:34:32 +0800

    1

--- swing.py
+++ swing.py
@@ -7,9 +7,10 @@ mpl.rcParams['legend.fontsize'] = 10
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-theta = np.linspace(-4 * np.pi, 4 * np.pi, 100)
-z = np.linspace(-2, 2, 100)
-r = z ** 2 + 1
+theta = np.linspace(-4 * np.pi, 4 * np.pi, 1000)
+z = np.linspace(-2, 2, 1000)
+# r = z ** 2 + 1
+r = 3
 x = r * np.sin(theta)
 y = r * np.cos(theta)
 ax.plot(x, y, z, label='parametric curve')
