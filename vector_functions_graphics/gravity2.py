be56362e0e54648d81db2b06550808bde301a05d
bjh <18566300@qq.com>
2020-02-02 21:15:37 +0800

    11

--- vector_functions_graphics/gravity2.py
+++ vector_functions_graphics/gravity2.py
@@ -14,20 +14,47 @@ def plot_grid(x, y, ax=None, **kwargs):
     ax.autoscale()
 
 
+def sig(i):
+    return -1 if (i < 0) else 1
+
+
 def f(x, y):
     u = x
     v = y
-    # u = x + 2
-    # v = y + 2
-    return u + np.exp(-u ** 2 - v ** 2), v + np.exp(-u ** 2 - v ** 2)
+    return u - np.exp(-u ** 2 - v ** 2), v - np.exp(-u ** 2 - v ** 2)
+    # return u - np.exp(-u**2), v - np.exp(-v ** 2)
+
+
+def f1(x: np.array, y: np.array):
+    u = []
+    v = []
+    for i in range(0, len(x)):
+        ui = []
+        vi = []
+        for j in range(0, len(x[i])):
+            print(x[i][j])
+            ui.append(x[i][j] + sig(x[i][j]) * np.exp(-x[i][j]**2 - y[i][j]**2))
+            vi.append(y[i][j] + sig(y[i][j]) * np.exp(-x[i][j]**2 - y[i][j]**2))
+
+        u.append(ui)
+        v.append(vi)
+    return u, v
 
 
 fig, ax = plt.subplots()
 ax.set_aspect('equal')
+# 需要弄清楚这里产生的是什么结果？不是一个二维数组！
 grid_x, grid_y = np.meshgrid(np.linspace(-3, 3, 20), np.linspace(-3, 3, 20))
+print(grid_x)
+print("-----------------")
+print(grid_y)
 plot_grid(grid_x, grid_y, ax=ax, color="lightgrey")
 
-distx, disty = f(grid_x, grid_y)
+distx, disty = f1(grid_x, grid_y)
 plot_grid(distx, disty, ax=ax, color="C0")
+
+# distx, disty = f1(grid_x, grid_y)
+# plot_grid(distx, disty, ax=ax, color="C0")
+
 
 plt.show()
